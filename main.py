f47025e61de0df082938f4df4d99d76670865abb
Jason Roberts <jason@Jasons-MacBook-Pro.local>
2021-04-26 21:26:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+adjective_one = input("Name an adjective: ")
+plural_noun_one = input("Name a plural noun: ")
+plural_noun_two = input("Name a second plural noun: ")
+silly_word = input("Name a silly word: ")
+type_of_liquid = input("Name a type of liquid: ")
+adjective_two = input("Name a second adjective: ")
+noun = input("Name a noun: ")
+verb = input("Name a verb: ")
+plural_noun_three = input("Name a third plural noun: ")
+verb_ing = input("Name a verb ending in -ing: ")
+verb_ing_two = input("Name a second verb ending in -ing: ")
+number = input("Please give a number: ")
+plural_noun_four = input("Name a fourth plural noun: ")
+noun_2 = input("Name a second noun: ")
+
+mad_libs = f"American children are fascinated by {adjective_one} stuff-like stories that scare the\n" \
+           f" {plural_noun_one} off them or make their {plural_noun_two} stand on end. Scientists say\n" \
+           f" this is because being frightened causes the {silly_word} gland to function and puts {type_of_liquid}\n" \
+           f"into their blood. And everyone knows that kids feel {adjective_two}. When they are scared by a movie \n" \
+           f"or a/an {noun}, boys laugh and holler and {verb}. but girls cover their eyes with their {plural_noun_three}\n" \
+           f"and keep screaming and {verb_ing}. Most kids get over this by the time they are {number} years old. \n" \
+           f"Then they like movies about cars {verb_ing_two} or cops shooting {plural_noun_four} or, if they are girls\n" \
+           f", they like movies about a boy meeting a/an {noun_2} and falling in love. Of course, that can be scary too.\n"
+
+print(mad_libs)
